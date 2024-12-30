028e399595dfb74dbcfd9b26b16c945187854ee5
Uriel Faran <digitalmnz001@gmail.com>
2024-12-30 21:39:04 +0200

    add is favorite

--- app/components/display-data/DisplayBoard.tsx
+++ app/components/display-data/DisplayBoard.tsx
@@ -3,6 +3,8 @@ import { Link } from "react-router";
 import BoardActionDropdown from "../dropdowns/BoardActionDropdown";
 import { Card } from "../ui/card";
 import UpdateBoardInput from "../UpdateBoardInput";
+import { Star } from "lucide-react";
+import { Button } from "../ui/button";
 
 export type BoardWithLists = Prisma.BoardGetPayload<{
   include: {
@@ -15,20 +17,21 @@ interface DisplayListProps {
 }
 
 function DisplayBoard({ board }: DisplayListProps) {
+  console.log(board)
   const bg = `${board.backgroundColor ?? "secondary"}`;
   return (
     <Card
-      className={"min-w-72 min-h-28 h-fit cursor-pointer group relative"}
-      style={{
-        background: bg.startsWith("linear-gradient")
-          ? bg
-          : `var(--color-${bg})`,
-      }}
+    className={"min-w-72 min-h-28 h-fit cursor-pointer group relative"}
+    style={{
+      background: bg.startsWith("linear-gradient")
+      ? bg
+      : `var(--color-${bg})`,
+    }}
     >
       <Link
         to={`/board/${board.id}/${board.name}`}
         className="absolute inset-0 z-0"
-      />
+        />
       <div className="p-2 flex flex-row justify-between bg-transparent">
         <div onClick={(e) => e.stopPropagation()} className="relative z-10">
           <UpdateBoardInput board={board} />
@@ -40,6 +43,9 @@ function DisplayBoard({ board }: DisplayListProps) {
           <BoardActionDropdown boardId={board.id} />
         </div>
       </div>
+      <Button className="flex right-1 bottom-1" variant={'ghost'} size={'sm'}>
+        <Star className={`${board.isFavorite ? 'text-yellow-400' : ''}`}/>
+      </Button>
     </Card>
   );
 }
---
+++ prisma/migrations/20241230193151_add_fovorite/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `board` ADD COLUMN `isFavorite` BOOLEAN NOT NULL DEFAULT false;
