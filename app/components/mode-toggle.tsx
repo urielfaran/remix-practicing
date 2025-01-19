c0eec226a4c454651624247f305c0ef56a82fed8
Uriel Faran <digitalmnz001@gmail.com>
2025-01-19 20:15:22 +0200

    fix toggle

--- app/components/mode-toggle.tsx
+++ app/components/mode-toggle.tsx
@@ -1,46 +1,20 @@
 import { Moon, Sun } from "lucide-react";
 import { Theme, useTheme } from "remix-themes";
 
-import { Button } from "./ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
 import { Switch } from "./ui/switch";
-import { Label } from "./ui/label";
 
 export function ModeToggle() {
   const [theme, setTheme] = useTheme();
-console.log(theme, Theme.LIGHT);
   return (
-    // <DropdownMenu>
-    //   <DropdownMenuTrigger asChild>
-    //     <Button variant="ghost" size="icon">
-    //       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-    //       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-    //       <span className="sr-only">Toggle theme</span>
-    //     </Button>
-    //   </DropdownMenuTrigger>
-    //   <DropdownMenuContent align="end">
-    //     <DropdownMenuItem onClick={() => setTheme(Theme.LIGHT)}>
-    //       Light
-    //     </DropdownMenuItem>
-    //     <DropdownMenuItem onClick={() => setTheme(Theme.DARK)}>
-    //       Dark
-    //     </DropdownMenuItem>
-    //   </DropdownMenuContent>
-    // </DropdownMenu>
     <div className="flex items-center space-x-2">
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90" />
+      <Sun className="h-[1rem] w-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90" />
       <Switch
         id="theme"
         onCheckedChange={() =>
           setTheme(theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT)
         }
       />
-      <Moon className=" h-[1.2rem] w-[1.2rem] transition-all dark:rotate-0 dark: -rotate-0" />
+      <Moon className=" h-[1rem] w-[1rem] transition-all dark:rotate-0 dark: -rotate-0" />
     </div>
   );
 }
